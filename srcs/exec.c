134e5c32698121589fa976c6093832842749c13a
Adrien CONSTANTIN <adconsta@e2r2p4.clusters.42paris.fr>
2022-05-14 12:54:11 +0200

    auto-commit

---
+++ srcs/exec.c
@@ -0,0 +1,24 @@
+
+
+
+typedef struct s_word {
+    char    letter;
+    int     start;
+    int     end;
+} t_word;
+
+void    init_word_index(t_word *word_index)
+{
+        
+}
+
+int main(int ac, char **av)
+{
+    int word_count = 0;
+    t_word  word_index[25];
+    char ** word_table;
+    init_word_index(word_index)
+    (void)av;
+    (void)ac;
+    return 0;
+}
